116cd52415279268f7b96d2c0d104d45e0ce740d
Muhammad Saifuddin <saif.outwit@gmail.com>
2016-10-27 17:31:30 +0400

    Update bundle.js

--- site/bundle.js
+++ site/bundle.js
@@ -495,9 +495,9 @@ document.getElementById('me').onclick = function() {
                 shadowAnchor: [4, 62],  // the same for the shadow
                 popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
             });
-            L.marker([pos.coords.latitude.round(3) + 001, pos.coords.longitude.round(3) + 001], {icon: educationIcon}).addTo(map).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-            L.marker([pos.coords.latitude.round(3) + 010, pos.coords.longitude.round(3) + 011], {icon: buildingIcon}).addTo(map).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-            L.marker([pos.coords.latitude.round(3) + 010, pos.coords.longitude.round(3) + 011], {icon: greenIcon}).addTo(map).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+            L.marker([pos.coords.latitude.round(3), pos.coords.longitude.round(3) + 001], {icon: educationIcon}).addTo(map).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+            L.marker([pos.coords.latitude.round(3), pos.coords.longitude.round(3) + 011], {icon: buildingIcon}).addTo(map).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+            L.marker([pos.coords.latitude.round(3), pos.coords.longitude.round(3) + 011], {icon: greenIcon}).addTo(map).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
             
             document.getElementById('me').innerHTML = res[0].feature.properties.LABEL_E;
             
